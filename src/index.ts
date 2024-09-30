849c002f0c8048ef290c70fc45b98961e9b40ab3
Rahul <caulirahul6@gmail.com>
2024-09-30 21:52:11 +0530

    feat: zod types and schemas

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, { Express, Request, Response } from "express";
+import { userRouter } from "./routes";
+import { validateData } from "./middleware/validationMiddleware";
+import { userRegistrationSchema } from "./schemas/userSchema";
 
 const app: Express = express();
 const port = 3000;
+app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.use("/api", validateData(userRegistrationSchema), userRouter);
 
 app.listen(port, () => {
   console.log(` Server is running at http://localhost:${3000}`);
