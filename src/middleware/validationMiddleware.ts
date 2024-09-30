485e9dc4d2ba910a5ee086ae0f3b90534b9d34ac
Rahul <caulirahul6@gmail.com>
2024-09-30 21:51:32 +0530

    feat: validation middleware

---
+++ src/middleware/validationMiddleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { z, ZodError } from "zod";
+
+import { StatusCodes } from "http-status-codes";
+
+export function validateData(schema: z.ZodObject<any, any>) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log(req.body);
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((err: any) => ({
+          message: `${err.path.join(".")} is ${err.message}`,
+        }));
+        console.log(error);
+        res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: "Invalid data", details: errorMessages });
+      } else {
+        console.log(error);
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: "Internal Server Error" });
+      }
+    }
+  };
+}
