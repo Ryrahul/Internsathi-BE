8c61a34b6e76d230d4610b0a872346e36d65e6b3
Rahul <caulirahul6@gmail.com>
2024-10-26 00:35:20 +0530

    fix: make schema type comaptible

--- src/schemas/companySchema.ts
+++ src/schemas/companySchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const CompanySchema = z.object({
   about: z.string().min(1, "About field is required"),
   category: z.string().min(1, "Category is required"),
-  company: z.string().min(1, "Company name is required"),
+  company_name: z.string().min(1, "Company name is required"),
   founded: z.string(),
   location: z.string().min(1, "Location is required"),
   address: z.string().min(1, "Address is required"),
