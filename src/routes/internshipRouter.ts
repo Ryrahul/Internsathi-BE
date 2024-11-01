ccc4a82370be95af1ca532880e7bc76fbecc4eb8
Rahul <caulirahul6@gmail.com>
2024-11-02 02:54:15 +0530

    feat: my-internships route

--- src/routes/internshipRouter.ts
+++ src/routes/internshipRouter.ts
@@ -3,14 +3,22 @@ import {
   appliedUsers,
   createInternship,
   getInternshipsDetails,
+  myInternship,
 } from "../controller";
 import { validateData } from "../middleware/validationMiddleware";
 import { companyAuth } from "../middleware/company";
 import { applyForInternship } from "../controller/internshipApply";
 import { jwtAuth } from "../middleware/verifyToken";
+import InternshipSchema from "../schemas/internshipSchema";
 
 export const internshipRouter = express.Router();
-internshipRouter.post("/post", companyAuth, createInternship);
+internshipRouter.post(
+  "/post",
+  companyAuth,
+  validateData(InternshipSchema),
+  createInternship
+);
 internshipRouter.post("/apply/:id", jwtAuth, applyForInternship);
 internshipRouter.get("", jwtAuth, getInternshipsDetails);
 internshipRouter.get("/applicants/:id", companyAuth, appliedUsers);
+internshipRouter.get("/my-postings", companyAuth, myInternship);
